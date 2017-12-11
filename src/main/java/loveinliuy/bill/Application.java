9000cf329fcdeb95076dec55c4317359c9339bdc
zhangshibo <zhangshibo@thunisoft.com>
2017-12-11 21:03:59 +0800

    Initial commit

---
+++ src/main/java/loveinliuy/bill/Application.java
@@ -0,0 +1,24 @@
+package loveinliuy.bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class Application {
+
+    @RequestMapping("")
+    public String index(){
+
+        return "index";
+    }
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
