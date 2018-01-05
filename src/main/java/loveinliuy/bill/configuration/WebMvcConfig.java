6c3dd8daf9dbc6c76f10164a86a7244bd2af865a
zhangshibo <zhangshibo@thunisoft.com>
2018-01-05 17:32:37 +0800

    修复静态资源无法使用cache-control

--- src/main/java/loveinliuy/bill/configuration/WebMvcConfig.java
+++ src/main/java/loveinliuy/bill/configuration/WebMvcConfig.java
@@ -1,8 +1,10 @@
 package loveinliuy.bill.configuration;
 
+import loveinliuy.bill.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,12 +19,24 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Autowired
     private CommonInfoInterceptor commonInfoInterceptor;
 
+    @Autowired
+    private AppConfig config;
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/index");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/error").setViewName("error");
         registry.addViewController("/message").setViewName("message");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if(!config.isDebug()) {
+            registry.addResourceHandler("/**")
+                    .addResourceLocations("classpath:/static/")
+                    .setCachePeriod(24 * 60 * 60);
+        }
     }
 
     @Override
--- src/main/java/loveinliuy/bill/configuration/WebSecurityConfig.java
+++ src/main/java/loveinliuy/bill/configuration/WebSecurityConfig.java
@@ -26,6 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/login").permitAll()
                 .and()
                 .logout().permitAll();
+        http.headers().defaultsDisabled().cacheControl();
     }
 
     @Autowired
